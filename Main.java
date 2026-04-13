65e175c52c28115a3204c80f6fb9db76cd8e58fb
HarrisW <hywong796-c@my.cityu.edu.hk>
2026-04-13 23:38:36 +0800

    Additional updates with same purpose of last commit

--- Main.java
+++ Main.java
@@ -4,11 +4,32 @@ import data.Library;
 import java.util.*;
 
 public class Main {
-    public static ArrayList<Library> libraries = new ArrayList<>();
-    public static ArrayList<BookRecord> bookRecords = new ArrayList<>();
-    public static ArrayList<BookCopy> bokCopies = new ArrayList<>();
-
     public static void main(String[] args) {
-        
+        //start by loading data from files
+        while (true) {
+            System.out.println("If you are user, please type 1; Otherwise, please type 2");
+            Scanner in = new Scanner(System.in);
+            int userCode = in.nextInt();
+            if (!(userCode == 1 || userCode == 2)) {
+                System.out.println("Invalid user type, please type again");
+            }
+            else {
+                while (true) {
+                    System.out.print("Please type your command: ");
+                    String userCommand = in.nextLine();
+                    switch (userCommand) {
+                        //command handling block
+                        case "exit" -> {
+                            break;
+                        }
+
+                        default -> {
+                            System.out.println("Invalid user command. Please type again");
+                        }
+                    }
+
+                }
+            }
+        }
     }
 }
